14dd3616be7b70896c115f3569bb270a25ff610e
Steffen Harnack <s.harnack@mail.de>
2017-08-30 11:40:33 +0200

    finished basic functionality

--- bmi-rechner/mycalc.js
+++ bmi-rechner/mycalc.js
@@ -1,13 +1,7 @@
 "use strict";
 
 // Formel: bmi = körpermasse / (körperhöhe in meter) ^ 2
-
-// immer, wenn ein Wert in einem der beiden Eingabefelder verändert wird,
-// errechnet sich der BMI automatisch und wird auf der Seite angezeigt
-
-// variablen für Felder definieren
-// eventListener onChange auf Felder anwenden
-//		
+	
 
 window.addEventListener("load", function() {
    
@@ -30,4 +24,8 @@ window.addEventListener("load", function() {
 
 	weightObj.addEventListener("change", calcBMI);
 	heightObj.addEventListener("change", calcBMI);
+	weightObj.addEventListener("keyup", calcBMI);
+	heightObj.addEventListener("keyup", calcBMI);
+
+
 });
